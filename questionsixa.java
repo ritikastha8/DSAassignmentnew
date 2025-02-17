1180d14c424a691b04cd09c2ed09757aca2fce7d
Ritika Shrestha <shrestharitika998@gmail.com>
2025-02-17 17:49:37 +0545

    Question 6a added

---
+++ questionsixa.java
@@ -0,0 +1,84 @@
+import java.util.concurrent.Semaphore;
+
+class NumberPrinter {
+    public void printZero() {
+        System.out.print("0");
+    }
+    
+    public void printEven(int num) {
+        System.out.print(num);
+    }
+    
+    public void printOdd(int num) {
+        System.out.print(num);
+    }
+}
+
+class ThreadController {
+    private int n;
+    private NumberPrinter printer;
+    private Semaphore zeroSemaphore = new Semaphore(1);
+    private Semaphore evenSemaphore = new Semaphore(0);
+    private Semaphore oddSemaphore = new Semaphore(0);
+
+    public ThreadController(int n, NumberPrinter printer) {
+        this.n = n;
+        this.printer = printer;
+    }
+
+    public void printSequence() {
+        Thread zeroThread = new Thread(() -> {
+            try {
+                for (int i = 1; i <= n; i++) {
+                    zeroSemaphore.acquire();
+                    printer.printZero();
+                    if (i % 2 == 0) {
+                        evenSemaphore.release();
+                    } else {
+                        oddSemaphore.release();
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        Thread evenThread = new Thread(() -> {
+            try {
+                for (int i = 2; i <= n; i += 2) {
+                    evenSemaphore.acquire();
+                    printer.printEven(i);
+                    zeroSemaphore.release();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        Thread oddThread = new Thread(() -> {
+            try {
+                for (int i = 1; i <= n; i += 2) {
+                    oddSemaphore.acquire();
+                    printer.printOdd(i);
+                    zeroSemaphore.release();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        zeroThread.start();
+        evenThread.start();
+        oddThread.start();
+    }
+}
+
+public class questionsixa {
+    public static void main(String[] args) {
+        int n = 5;
+        NumberPrinter printer = new NumberPrinter();
+        ThreadController controller = new ThreadController(n, printer);
+        controller.printSequence();
+    }
+}
+
